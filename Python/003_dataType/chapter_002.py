b5352718db040c306981c9014177345ef887c801
Shahriar-em0n <contactshahriar12@gmail.com>
2026-01-05 19:57:30 +0600

    Add the mutable and immutable code

---
+++ Python/003_dataType/chapter_002.py
@@ -0,0 +1,11 @@
+#immutable
+
+suger_amount = 2
+print(f"Initial sugar:{suger_amount}")
+
+suger_amount = 12
+print(f"Inital sugar:{suger_amount}")
+
+# check there identity
+print(f"The id of 2 : {id(2)}")
+print(f"The id of 12 : {id(12)}")
---
+++ Python/003_dataType/chapter_003.py
@@ -0,0 +1,9 @@
+#mutable
+
+spice_mix = set()
+print(f"Initial speice mix id: {id(spice_mix)}")
+
+spice_mix.add("Ginger")
+spice_mix.add("cardamom")
+
+print(f"after speice mix id: {id(spice_mix)}")
