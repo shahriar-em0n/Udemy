da45e9b1bbd06f1a44aa0d8e130c5f95d6722f63
Shahriar-em0n <contactshahriar12@gmail.com>
2026-01-06 20:45:02 +0600

    J

--- Python/003_dataType/chapter_003.py
+++ Python/003_dataType/chapter_003.py
@@ -2,8 +2,12 @@
 
 spice_mix = set()
 print(f"Initial speice mix id: {id(spice_mix)}")
+print(f"Initial speice mix id: {spice_mix}")
 
 spice_mix.add("Ginger")
 spice_mix.add("cardamom")
 
+print(f"after speice mix id: {spice_mix}")
+
 print(f"after speice mix id: {id(spice_mix)}")
+ 
