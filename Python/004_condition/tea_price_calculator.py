cc3a0e8290373f339cdcca82a8a4cd1e4e320d4c
Shahriar-em0n <contactshahriar12@gmail.com>
2026-02-13 23:31:06 +0600

    Add code

---
+++ Python/004_condition/tea_price_calculator.py
@@ -0,0 +1,10 @@
+cup = input("Choose your cup size (small, medium, large): ").lower()
+
+if cup == "small":
+    print("Price is 10 Taka")
+elif cup == "medium":
+    print("price is 15 Taka")
+elif cup == "large":
+    print("price is 20 Taka")
+else:
+    print("Unknown cup size.")
